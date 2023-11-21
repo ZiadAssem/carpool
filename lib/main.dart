47f4adc8b102bd5d52184762809e0c0773ce768b
ZiadAssem <ziad.assem2001@gmail.com>
2023-11-21 14:32:55 +0200

    refactored bottom nav page

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 // import 'package:carpool/firebase_options.dart';
 // import 'package:carpool/routes.dart';
 // import 'package:firebase_core/firebase_core.dart';
+import 'package:carpool/view/main_container.dart';
 import 'package:flutter/material.dart';
 
 import 'view/sign_in.dart';
@@ -18,7 +19,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return  MaterialApp(
-      home: SignInScreen(),
+      home: BottomNavPage(),
     );
   }
 }
--- lib/view/main_container.dart
+++ lib/view/main_container.dart
@@ -15,67 +15,62 @@ class _BottomNavPageState extends State<BottomNavPage> {
 
   final List<Widget> _pages = [
     const Routes(),
-    const CartPage(), 
-    ProfilePage(), 
+    const CartPage(),
+    ProfilePage(),
   ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        decoration: const BoxDecoration(
-          color: Colors.white,
-          image: DecorationImage(
-            image: AssetImage("assets/images/car-background.jpg"),
-            fit: BoxFit.fitWidth,
-          ),
-        ),
-        child: PageView(
-          controller: _pageController,
-          onPageChanged: (index) {
-            setState(() {
-              _currentIndex = index;
-            });
-          },
-          children: _pages,
-        ),
-      ),
+      body: _buildPageView(),
       bottomNavigationBar: _buildBottomNavigationBar(),
     );
   }
 
-  Widget _buildBottomNavigationBar(){
+  Widget _buildBottomNavigationBar() {
     return BottomNavigationBar(
-        unselectedItemColor: Colors.black,
-        selectedItemColor: const Color.fromARGB(255, 142, 15, 6),
-        backgroundColor: Colors.white, // Set background color
-        currentIndex: _currentIndex,
-        onTap: (index) {
-          setState(() {
-            _currentIndex = index;
-            _pageController.animateToPage(
-              index,
+      unselectedItemColor: Colors.black,
+      selectedItemColor: const Color.fromARGB(255, 142, 15, 6),
+      backgroundColor: Colors.white, // Set background color
+      currentIndex: _currentIndex,
+      onTap: (index) {
+        setState(() {
+          _currentIndex = index;
+          _pageController.animateToPage(index,
               duration: const Duration(milliseconds: 300),
-              curve: Curves.easeInOut,
-            );
-          });
-        },
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.map),
-            label: 'Routes',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.shopping_cart),
-            label: 'Cart',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            label: 'Profile',
-          ),
-        ],
-      );
+              curve: Curves.easeInOut);
+        });
+      },
+      items: const [
+        BottomNavigationBarItem(icon: Icon(Icons.map), label: 'Routes'),
+        BottomNavigationBarItem(icon: Icon(Icons.shopping_cart), label: 'Cart'),
+        BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
+      ],
+    );
   }
 
+  Widget _buildPageView() {
+    return Container(
+      decoration: _buildMainBackground(),
+      child: PageView(
+        controller: _pageController,
+        onPageChanged: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        children: _pages,
+      ),
+    );
+  }
 
+  BoxDecoration _buildMainBackground() {
+    return const BoxDecoration(
+      color: Colors.white,
+      image: DecorationImage(
+        image: AssetImage("assets/images/car-background.jpg"),
+        fit: BoxFit.fitWidth,
+      ),
+    );
+  }
 }
